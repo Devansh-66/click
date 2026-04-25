c0043a11d7089e8fd1066e2935f136d104dc28d0
Devansh Rajput <devanshrajput960@gmail.com>
2026-04-25 18:24:58 +0530

    Add test.sh script for running pytest with options

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+OUTPUT=""
+MODE=""
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --output_path)
+      OUTPUT="$2"
+      shift 2
+      ;;
+    base|new)
+      MODE="$1"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+if [ -z "$OUTPUT" ]; then
+  OUTPUT="results.xml"
+fi
+
+if [ "$MODE" = "base" ]; then
+  pytest tests/test_types.py --junitxml="$OUTPUT"
+else
+  pytest tests/test_choice_canonical.py --junitxml="$OUTPUT"
+fi
