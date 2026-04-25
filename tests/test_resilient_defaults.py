0e07aefb4a90edacbeaa6d9a424a2137e9771441
Devansh Rajput <devanshrajput960@gmail.com>
2026-04-25 21:41:05 +0530

    Rename test for callable default in resilient parsing

--- tests/test_resilient_defaults.py
+++ tests/test_resilient_defaults.py
@@ -1,7 +1,7 @@
 import click
 
 
-def test_default_callback_not_called_in_resilient_parsing():
+def test_callable_default_not_called_in_resilient_parsing():
     called = {"count": 0}
 
     def default():
@@ -9,9 +9,34 @@ def test_default_callback_not_called_in_resilient_parsing():
         return "value"
 
     ctx = click.Context(click.Command("cmd"), resilient_parsing=True)
-
     option = click.Option(["--name"], default=default)
 
-    option.get_default(ctx)
+    result = option.get_default(ctx)
 
     assert called["count"] == 0
+    assert callable(result)
+
+
+def test_callable_default_called_in_normal_parsing():
+    called = {"count": 0}
+
+    def default():
+        called["count"] += 1
+        return "value"
+
+    ctx = click.Context(click.Command("cmd"))
+    option = click.Option(["--name"], default=default)
+
+    result = option.get_default(ctx)
+
+    assert called["count"] == 1
+    assert result == "value"
+
+
+def test_non_callable_default_unchanged():
+    ctx = click.Context(click.Command("cmd"))
+    option = click.Option(["--name"], default="hello")
+
+    result = option.get_default(ctx)
+
+    assert result == "hello"
